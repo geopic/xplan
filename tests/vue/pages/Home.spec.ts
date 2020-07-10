04516cf216753251f525b2542b27fe9963119e67
George Pickering <29524044+geopic@users.noreply.github.com>
2020-07-10 16:53:50 +0100

    Fix Home tests

--- tests/vue/pages/Home.spec.ts
+++ tests/vue/pages/Home.spec.ts
@@ -1,11 +1,13 @@
-import { shallowMount } from '@vue/test-utils';
+import { mount } from '@vue/test-utils';
 import Home from '@/vue/pages/Home.vue';
 import { PlanData } from '@/common/types';
 import { v4 as uuid } from 'uuid';
+import props from '@/common/props';
+import utils from '@/common/utils';
 
 describe('Home', () => {
   describe('no data exists', () => {
-    const wrapper = shallowMount(Home, { propsData: { data: [] } });
+    const wrapper = mount(Home);
 
     test('renders an intro element', () => {
       expect(() => wrapper.get('#site-intro')).not.toThrow();
@@ -20,29 +22,37 @@ describe('Home', () => {
     });
   });
 
-  describe.skip('data exists', () => {
+  describe('data exists', () => {
     describe('unlimited number of days', () => {
-      const plan: PlanData = {
-        id: uuid(),
-        title: 'Write unit tests',
-        isUnlimited: false,
-        squares: [
-          {
-            id: uuid(),
-            date: new Date(),
-            isCompleted: false,
-            notes: null
-          }
-        ]
-      };
+      const data = utils.storage.default;
 
-      const wrapper = shallowMount(Home, { propsData: { plans: [plan] } });
+      data.plans = [
+        {
+          id: uuid(),
+          title: 'Write unit tests',
+          isUnlimited: true,
+          squares: [
+            {
+              id: uuid(),
+              date: new Date(),
+              isCompleted: false,
+              notes: null
+            }
+          ]
+        }
+      ];
+
+      localStorage.setItem(props.site.lsEntry, JSON.stringify(data));
+
+      const wrapper = mount(Home);
 
       test('renders one square', () => {
         expect(() => wrapper.get('.plan-square')).not.toThrow();
       });
 
-      test("renders 'unlimited' marker", () => {});
+      test("renders 'unlimited' marker", () => {
+        expect(() => wrapper.get('.plan-unlimited-box')).not.toThrow();
+      });
     });
 
     describe('non-unlimited number of days', () => {});
